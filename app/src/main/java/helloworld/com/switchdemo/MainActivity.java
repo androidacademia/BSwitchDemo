fa92330e6ae98e1ebc6fa0c24b4176822c590bc0
Girish Kumar Shakya <clairvoyant.1947@gmail.com>
2018-12-27 17:42:39 +0530

    Initial commit

---
+++ app/src/main/java/helloworld/com/switchdemo/MainActivity.java
@@ -0,0 +1,29 @@
+package helloworld.com.switchdemo;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.widget.CompoundButton;
+import android.widget.Switch;
+import android.widget.TextView;
+
+public class MainActivity extends AppCompatActivity {
+    private Switch aSwitch;
+    private TextView textView;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        aSwitch = findViewById(R.id.switchToggle);
+        textView = findViewById(R.id.textView);
+        aSwitch.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
+            @Override
+            public void onCheckedChanged(CompoundButton compoundButton, boolean b) {
+                if(b){
+                    textView.setText("Switch is ON");
+                }else {
+                    textView.setText("Switch is OFF");
+                }
+            }
+        });
+    }
+}
